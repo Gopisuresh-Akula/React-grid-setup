797ba28d7f9a28ae859327af34acab780ec52d5f
AGS <gopisureshakula123@gmail.com>
2020-09-09 14:45:57 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'
+
+const App=()=>{
+  return (
+    <>
+    
+
+
+
+    <div className="container">
+    <div className="item item-1">1:Orange</div>
+    <div className="item item-2">2:Green</div>
+    <div className="item item-3">3:violet</div>
+    <div className="item item-4">4:Pink</div>
+    <div className="item item-5">5:Blue</div>
+    <div className="item item-6">6:Brown</div>
+    </div>
+    </>
+  )
+}
+
+export default App  
